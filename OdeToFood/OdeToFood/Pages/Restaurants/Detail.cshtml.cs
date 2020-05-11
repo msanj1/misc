d1b215aadddc2f1a46e96a465cbc8037fe565823
Mohsen Sanjari <mohsen.sanjari@outlook.com>
2020-05-11 15:20:06 +1000

    Example project written in ASPNET Core using Razor Pages

---
+++ OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IRestaurantData _restaurantData;
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+        public Restaurant Restaurant { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = _restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
---
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IRestaurantData _restaurantData;
+        private readonly ILogger<ListModel> _logger;
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchTerm { get; set; }
+
+        public ListModel(IConfiguration configuration, IRestaurantData restaurantData,
+            ILogger<ListModel> logger)
+        {
+            _configuration = configuration;
+            _restaurantData = restaurantData;
+            _logger = logger;
+        }
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            _logger.LogError("Executing ListModel");
+            Message = _configuration["Message"];
+            Restaurants = _restaurantData.GetRestaurantsByName(SearchTerm);
+        }
+    }
+}
